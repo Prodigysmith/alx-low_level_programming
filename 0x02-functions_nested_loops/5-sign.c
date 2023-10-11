ade7b9dbded5ce475247c9041baf520c839d4b2f
Elijah <Elijah>
2023-10-10 18:19:18 -0700

    its done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,19 +7,25 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
-int print_sign(int n)
-{
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	} else if (n == 0)
+
+int _putchar(char c) {
+    return write(1, &c, 1);
 }
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
-	}
-		return (-1);
+
+int print_sign(int n) {
+    if (n > 0) {
+        _putchar('+');
+        return 1;
+    } else if (n == 0) {
+        _putchar('0');
+        return 0;
+    } else {
+        _putchar('-');
+        return -1;
+    }
+}
+
+int main(void) {
+    int result = print_sign(5);
+    return (0);
 }
